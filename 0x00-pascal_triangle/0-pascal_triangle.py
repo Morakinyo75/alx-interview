932b3343965d24eb148b1bc146b0bc31bab71c9e
Morakinyo75 <akinwandeemmanuel899@gmail.com>
2023-06-29 13:26:14 -0700

    0x00-pascal_triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)  # Initialize each row with 1's
+        if i > 1:
+            prev_row = triangle[i - 1]
+            for j in range(1, i):
+                row[j] = prev_row[j - 1] + prev_row[j]
+        triangle.append(row)
+    return triangle
